53e91771709a4ed3505b43e205c64635594bb1d4
Alexei Garban <garban.valdeon@gmail.com>
2023-07-10 16:41:18 +0200

    data governance added

---
+++ src/api/data-governance/routes/data-governance.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * data-governance router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::data-governance.data-governance');

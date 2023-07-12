13a296196346811446b53b154eef375e6bd1f5d9
Alexei Garban <garban.valdeon@gmail.com>
2023-07-12 15:52:25 +0200

    act added

---
+++ src/api/act/controllers/act.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * act controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::act.act');
---
+++ src/api/act/routes/act.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * act router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::act.act');

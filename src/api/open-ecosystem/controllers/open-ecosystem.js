5af7045cbffdbe01b36465b94abe2b2d77635236
Alexei Garban <garban.valdeon@gmail.com>
2023-08-02 12:07:19 +0200

    open ecosystems

---
+++ src/api/open-ecosystem/controllers/open-ecosystem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * open-ecosystem controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::open-ecosystem.open-ecosystem');

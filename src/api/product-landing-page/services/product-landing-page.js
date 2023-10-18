242ff443708d59600d173aa963a8aef730ec4b23
Alexei Garban <garban.valdeon@gmail.com>
2023-10-18 11:36:53 +0200

    product landing page

---
+++ src/api/product-landing-page/services/product-landing-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-landing-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-landing-page.product-landing-page');

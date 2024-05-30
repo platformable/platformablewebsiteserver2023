e190578d69f26eb95bdfc2b34dbd699d6113af33
Alexei Garban <garban.valdeon@gmail.com>
2024-05-30 12:51:04 +0200

    ob pages

---
+++ src/api/ob-products-page/routes/ob-products-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ob-products-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ob-products-page.ob-products-page');
